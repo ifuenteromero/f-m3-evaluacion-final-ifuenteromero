da26b44a2fc6979130e945584bb748dbbd21465e
Irene Fuente <ifuenteromero@gmail.com>
2019-05-24 15:51:39 +0200

    Initial commit

---
+++ docs/precache-manifest.04721fa8b9be2ef0f1099fb903a35fd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277d2a0e01ae403f0df0c6c80820c5fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8089ce6fd3101b31ef4",
+    "url": "./static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "./static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b8089ce6fd3101b31ef4",
+    "url": "./static/js/main.a3c07769.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
