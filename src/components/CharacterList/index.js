7ba46cc2c2567dea0c2402d85096c6d3381fb4a5
Irene Fuente <ifuenteromero@gmail.com>
2019-05-24 18:10:09 +0200

    Convert CharacterList to dummie

--- src/components/CharacterList/index.js
+++ src/components/CharacterList/index.js
@@ -2,25 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CharacterCard from '../CharacterCard';
 
-class CharacterList extends React.Component{
-
-    render(){
-        const {characters} = this.props;
-        return(
-            <ul>{
-                characters.map(character =>
-                  <li key={character.id} >
-                    <CharacterCard character = {character} />
-                  </li>
-                )}
-              </ul>
-        );
-    }
+const CharacterList = props => {
+  const { characters } = props;
+  return (
+    <ul>{
+      characters.map(character =>
+        <li key={character.id} >
+          <CharacterCard character={character} />
+        </li>
+      )}
+    </ul>
+  );
 }
 
 CharacterList.propTypes = {
-  characters : PropTypes.arrayOf(PropTypes.object.isRequired).isRequired
+  characters: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired
 }
-
 
 export default CharacterList;
