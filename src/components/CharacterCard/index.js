e398da11fdd73f73e9e49c9dcbd7b99dc89bcb81
Irene Fuente <ifuenteromero@gmail.com>
2019-05-24 18:14:41 +0200

    Convert CharacterCard to dummie

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class CharacterCard extends React.Component {
-    render() {
-        const { name, image, house } = this.props.character;
-        return (
-            <div>
-                <img src={image} alt={name} />
-                <h1>{name}</h1>
-                <p>{house}</p>
-            </div>
-        );
-    }
+const CharacterCard = props => {
+    const { name, image, house } = props.character;
+    return (
+        <div>
+            <img src={image} alt={name} />
+            <h1>{name}</h1>
+            <p>{house}</p>
+        </div>
+    );
 }
 
 CharacterCard.propTypes = {
     character: PropTypes.shape({
-        id : PropTypes.number.isRequired,
-        image : PropTypes.string.isRequired,
-        name : PropTypes.string.isRequired,
-        house : PropTypes.string.isRequired
+        id: PropTypes.number.isRequired,
+        image: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        house: PropTypes.string.isRequired
     })
 }
 
