5970cd049befd71e32b657d1a9f85774817c879a
Irene Fuente <ifuenteromero@gmail.com>
2019-05-25 19:30:03 +0200

    update docs folder

---
+++ docs/precache-manifest.4f417cc8595f1668885384eb80bf175c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cc17f43c537ac19dfe586c8d2becbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78106b74f5fc0393beb1",
+    "url": "./static/css/main.a848f610.chunk.css"
+  },
+  {
+    "revision": "62495b0d7d673ed69b7b",
+    "url": "./static/js/2.ae1dbc10.chunk.js"
+  },
+  {
+    "revision": "78106b74f5fc0393beb1",
+    "url": "./static/js/main.b2b59822.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a634c60ad0a181a043e8aaee64c632d7.js"
+  "./precache-manifest.4f417cc8595f1668885384eb80bf175c.js"
 );
 
 self.addEventListener('message', (event) => {
