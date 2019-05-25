30fb786e60d2ce87cd4335a41671df1619e39f51
Irene Fuente <ifuenteromero@gmail.com>
2019-05-25 14:13:55 +0200

    clean some code

--- src/components/CharacterDetail/index.js
+++ src/components/CharacterDetail/index.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const CharacterDetail = props => {
     const { characters } = props;
     const { name, id, image, house, dob, patronus, alive } = characters[2];
-    const isAlive = alive ? 'vivo' : 'muerto'
+    const isAlive = alive ? 'alive' : 'dead'
     return (
         <div data-id={id}>
             <img src={image} alt={name} />
@@ -14,17 +14,15 @@ const CharacterDetail = props => {
             <p>{dob}</p>
             <p>{patronus}</p>
             <p>{isAlive}</p>
-
-        </div>)
-
+        </div>
+    )
 
 }
 
 CharacterDetail.propTypes = {
     characters: PropTypes.arrayOf(PropTypes.object),
-    id: PropTypes.number
-
 }
+
 export default CharacterDetail;
 
 
